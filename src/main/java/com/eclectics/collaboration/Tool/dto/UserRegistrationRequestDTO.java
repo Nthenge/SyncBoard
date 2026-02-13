1d4b46e5b48a1b3cf39a84b72d98975805739f22
Abraham Nthenge Mutinda <abrahamnetsec@gmail.com>
2026-02-13 11:41:18 +0300

    Remove hard coded constructor

--- src/main/java/com/eclectics/collaboration/Tool/dto/UserRegistrationRequestDTO.java
+++ src/main/java/com/eclectics/collaboration/Tool/dto/UserRegistrationRequestDTO.java
@@ -1,5 +1,14 @@
 package com.eclectics.collaboration.Tool.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserRegistrationRequestDTO {
 
     private String firstName;
@@ -8,54 +17,4 @@ public class UserRegistrationRequestDTO {
     private String avatarUrl;
     private String password;
 
-    public UserRegistrationRequestDTO() {
-    }
-
-    public UserRegistrationRequestDTO(String firstName,String avatarUrl, String sirName, String email, String password) {
-        this.firstName = firstName;
-        this.sirName = sirName;
-        this.email = email;
-        this.avatarUrl = avatarUrl;
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getSirName() {
-        return sirName;
-    }
-
-    public void setSirName(String sirName) {
-        this.sirName = sirName;
-    }
-
-    public String getAvatarUrl() {
-        return avatarUrl;
-    }
-
-    public void setAvatarUrl(String avatarUrl) {
-        this.avatarUrl = avatarUrl;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
