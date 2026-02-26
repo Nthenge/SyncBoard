ab17f0ea804ed6b313bd13312f1d5cce03892d4a
Abraham Nthenge Mutinda <abrahamnetsec@gmail.com>
2026-02-26 16:18:44 +0300

    Set frontend to security config

--- src/main/java/com/eclectics/collaboration/Tool/configuration/SecurityConfig.java
+++ src/main/java/com/eclectics/collaboration/Tool/configuration/SecurityConfig.java
@@ -33,8 +33,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-                "http://172.0.0.1:4200/",
-                "http://localhost:4200/"
+                "https://syncboard-frontend-814g.onrender.com"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
